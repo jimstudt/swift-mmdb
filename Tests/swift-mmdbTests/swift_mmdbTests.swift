1c7aef5fac7154ef8e0f78799497573819640b20
Jim Studt <jim@studt.net>
2022-04-17 16:34:44 -0500

    Initial Commit

---
+++ Tests/swift-mmdbTests/swift_mmdbTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swift_mmdb
+
+final class swift_mmdbTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swift_mmdb().text, "Hello, World!")
+    }
+}
