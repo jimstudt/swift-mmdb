28d58b04881625a16fbe4b1f44829e20d1ca4176
Jim Studt <jim@studt.net>
2022-04-18 09:00:33 -0500

    Clean up a little.

--- Sources/MMDB/MMDB.swift
+++ Sources/MMDB/MMDB.swift
@@ -1,5 +1,7 @@
 import Foundation
 public class MMDB {
+    
+    /// The data field type tag read from the MMDB file as given in *MaxMind DB File Format Specification 2.0*
     enum FieldType : UInt8 {
         case pointer = 1
         case string = 2
@@ -16,12 +18,85 @@ public class MMDB {
         case endMarker = 13
         case boolean = 14
         case float = 15
+        
+        
+        /// Scan a Value once the FieldType is known.
+        /// - Parameters:
+        ///   - bytes: The raw bytes, must be able to subscript from `pointer` to `pointer + size`.
+        ///   - size: The number of bytes to process.
+        /// - Returns: Read a `MMDB.Value` from a block of bytes. Does not work for `.map` and `.array`, only
+        ///   the simple types.
+        func scan( bytes: UnsafePointer<UInt8>, size: Int) -> Value? {
+            switch self {
+            case .pointer:
+                fatalError("Field type '\(self)' not supported")
+            case .string:
+                let u = (0..<size).map{ bytes[$0]}
+                guard let s = String(bytes: u, encoding: .utf8) else {
+                    return nil
+                }
+                return .string(s)
+            case .double:
+                let v : UnsafePointer<Double> = .init( OpaquePointer( bytes))
+                return .double(v.pointee)
+            case .bytes:
+                let v : [UInt8] = (0 ..< size).map{ bytes[$0]}
+                return .bytes( v)
+            case .uint16:
+                var r : UInt16 = 0
+                for i in 0 ..< size {
+                    r = (r << 8) | UInt16( bytes[i])
+                }
+                return .uint16( r)
+            case .uint32:
+                var r : UInt32 = 0
+                for i in 0 ..< size {
+                    r = (r << 8) | UInt32( bytes[i])
+                }
+                return .uint32( r)
+            case .uint64:
+                var r : UInt64 = 0
+                for i in 0 ..< size {
+                    r = (r << 8) | UInt64( bytes[i])
+                }
+                return .uint64( r)
+            case .int32:
+                fatalError("Field type '\(self)' not supported")
+            case .uint128:
+                fatalError("Field type '\(self)' not supported")
+            case .dataCacheContainer:
+                fatalError("Field type '\(self)' not supported")
+            case .endMarker:
+                fatalError("Field type '\(self)' not supported")
+            case .boolean:
+                fatalError("Field type '\(self)' not supported")
+            case .float:
+                fatalError("Field type '\(self)' not supported")
+                
+            //
+            // `map` and `array` need to be able to recursively call into the
+            // Store's readValue function. They have to be handled at a higher
+            // level.
+            //
+            case .map:
+                fatalError("Field type '.map' can not be scanned by FieldType")
+            case .array:
+                fatalError("Field type '.array' can not be scanned by FieldType")
+            }
+        }
     }
+    
+    
+    /// A *value* read from an MMDB file. This parallels the `FieldType` tags, but includes an associated value
+    /// and you can't mix rawValues and associated values
     indirect enum Value {
         case string(String)
         case map( [String:Value])
-        case uint( UInt)
-        case int( Int)
+        case uint16( UInt16)
+        case uint32( UInt32)
+        case uint64( UInt64)
+        case uint128( high: UInt64, low: UInt64)
+        case int32( Int32)
         case double( Double)
         case bytes( [UInt8] )
         case array( [Value])
@@ -59,6 +134,9 @@ public class MMDB {
         }
 
         func readValue( pointer: inout Int) -> Value? {
+            //
+            // Read the ControlByte
+            //
             let controlByte = bytes[pointer]
             pointer += 1
             
@@ -67,6 +145,9 @@ public class MMDB {
             let fieldType : FieldType
             let payloadSize : Int
 
+            //
+            // Decode the FieldType
+            //
             switch top3 {
             case 0:
                 guard let ft = FieldType(rawValue: bytes[pointer] + 7) else {
@@ -82,54 +163,28 @@ public class MMDB {
                 fieldType = ft
             }
             
+            //
+            // Decode the payloadSize
+            //
             switch payloadSize0 {
             case 29:
-                let p1 = bytes[pointer]
+                payloadSize = 29 + Int(bytes[pointer])
                 pointer += 1
-                payloadSize = 29 + Int(p1)
             case 30:
-                let p1 = bytes[pointer]
-                pointer += 1
-                let p2 = bytes[pointer]
-                pointer += 1
-                payloadSize = 285 + 256 * Int(p1) + Int(p2)
+                payloadSize = 285 + 256 * Int(bytes[pointer]) + Int(bytes[pointer+1])
+                pointer += 2
             case 31:
-                let p1 = bytes[pointer]
-                pointer += 1
-                let p2 = bytes[pointer]
-                pointer += 1
-                let p3 = bytes[pointer]
-                pointer += 1
-                payloadSize = 65821 + 65536 * Int(p1) + 256 * Int(p2) + Int(p3)
+                payloadSize = 65821 + 65536 * Int(bytes[pointer]) + 256 * Int(bytes[pointer+1]) + Int(bytes[pointer+2])
+                pointer += 3
             default:
                 payloadSize = Int(payloadSize0)
             }
-            
+
+            //
+            // Read the Value. map and array are special, we need to handle them so we can
+            // recurse. The rest of the field types are just scanned by their enum.
+            //
             switch fieldType {
-            case .pointer:
-                fatalError("Field type \(fieldType) not supported")
-            case .string:
-                let u = (0..<payloadSize).map{ bytes[pointer + $0]}
-                pointer += payloadSize
-                guard let s = String(bytes: u, encoding: .utf8) else {
-                    return nil
-                }
-                return .string(s)
-            case .double:
-                let v : UnsafePointer<Double> = .init( OpaquePointer( db.advanced(by: pointer)))
-                pointer += 8
-                return .double(v.pointee)
-            case .bytes:
-                let v : [UInt8] = (0 ..< payloadSize).map{ bytes[ pointer + $0]}
-                pointer += payloadSize
-                return .bytes( v)
-            case .uint16, .uint32, .uint64:
-                var r : UInt = 0
-                for i in 0 ..< payloadSize {
-                    r = 256 * r + UInt( bytes[pointer + i])
-                }
-                pointer += payloadSize
-                return .uint( r)
             case .map:
                 var result : [String:Value] = [:]
                 for _ in 0 ..< payloadSize {
@@ -142,10 +197,6 @@ public class MMDB {
                     result[key] = val
                 }
                 return .map(result)
-            case .int32:
-                fatalError("Field type \(fieldType) not supported")
-            case .uint128:
-                fatalError("Field type \(fieldType) not supported")
             case .array:
                 var result : [Value] = []
                 for _ in 0 ..< payloadSize {
@@ -155,14 +206,10 @@ public class MMDB {
                     result.append(val)
                 }
                 return .array(result)
-            case .dataCacheContainer:
-                fatalError("Field type \(fieldType) not supported")
-            case .endMarker:
-                fatalError("Field type \(fieldType) not supported")
-            case .boolean:
-                fatalError("Field type \(fieldType) not supported")
-            case .float:
-                fatalError("Field type \(fieldType) not supported")
+            default:
+                let r = fieldType.scan(bytes: bytes.advanced(by: pointer), size: payloadSize)
+                pointer += payloadSize
+                return r
             }
         }
 
@@ -194,8 +241,8 @@ public class MMDB {
             return nil
         }
         
-        guard case let .uint(major) = metadata["binary_format_major_version"],
-              case let .uint(minor) = metadata["binary_format_minor_version"] else {
+        guard case let .uint16(major) = metadata["binary_format_major_version"],
+              case let .uint16(minor) = metadata["binary_format_minor_version"] else {
             print("no major and minor version")
             return nil
         }
@@ -205,25 +252,25 @@ public class MMDB {
             return nil
         }
         
-        majorVersion = major
-        minorVersion = minor
+        majorVersion = UInt(major)
+        minorVersion = UInt(minor)
         
-        guard case let .uint(epoch) = metadata["build_epoch"],
-              case let .uint(ipVersion) = metadata["ip_version"],
-              case let .uint(recordSize) = metadata["record_size"],
-              case let .uint(nodeCount) = metadata["node_count"],
+        guard case let .uint64(epoch) = metadata["build_epoch"],
+              case let .uint16(ipVersion) = metadata["ip_version"],
+              case let .uint16(recordSize) = metadata["record_size"],
+              case let .uint32(nodeCount) = metadata["node_count"],
               case let .string(databaseType) = metadata["database_type"] else {
             print("Missing some metadata")
             return nil
         }
         
-        self.epoch = epoch
-        self.ipVersion = ipVersion
-        self.recordSize = recordSize
-        self.nodeCount = nodeCount
+        self.epoch = UInt(epoch)
+        self.ipVersion = UInt(ipVersion)
+        self.recordSize = UInt(recordSize)
+        self.nodeCount = UInt(nodeCount)
         self.databaseType = databaseType
         
-        searchTreeSize = ( ( recordSize * 2 ) / 8 ) * nodeCount
+        searchTreeSize = ( ( self.recordSize * 2 ) / 8 ) * self.nodeCount
     }
     
     public convenience init?( from: URL) {
